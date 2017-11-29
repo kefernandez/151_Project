787326bc7aad52459ec2ff46ca017d6883a1538b
Class Account <eecs151-aca@c125m-1.EECS.Berkeley.EDU>
2017-11-29 15:50:28 -0800

    wrote cache

--- src/Riscv141.v
+++ src/Riscv141.v
@@ -139,34 +139,7 @@ module Riscv141(
    //DM Assignments
    assign dcache_din = data_byte_out;
    assign DM_data = dcache_dout;
-   
-    /*controller ctrl(
-        .datapath_contents(datapath_contents),
-        .dpath_controls_i(dpath_controls_i),
-        .exec_controls_x(exec_controls_x),
-        .hazard_controls(hazard_controls)
-    );
-
-    datapath dpath(
-        .clk(clk),
-        .reset(reset),
-        .stall(stall),
-        .dpath_controls_i(dpath_controls_i),
-        .exec_controls_x(exec_controls_x),
-        .hazard_controls(hazard_controls),
-        .datapath_contents(datapath_contents),
-        .dcache_addr(dcache_addr),
-        .icache_addr(icache_addr),
-        .dcache_we(dcache_we),
-        .dcache_re(dcache_re),
-        .icache_re(icache_re),
-        .dcache_din(dcache_din),
-        .dcache_dout(dcache_dout),
-        .icache_dout(icache_dout),
-        .csr_tohost(csr)
-    );*/
-
-
+  
    //1
    mux_3x1 PC_mux(
 	//.clk(clk),
@@ -358,6 +331,7 @@ module Riscv141(
    //20
    pipeline1 pipeline(.clk(clk),
 		      .reset(reset),
+		      .stall(stall),
 		      .csrwi_imm_ID(csrwi_imm_ID),
 		      .immediate_branch_store_SE_ID(immediate_branch_store_SE_ID),
 		      .immediate_branch_store_SE_EX(immediate_branch_store_SE_EX),
